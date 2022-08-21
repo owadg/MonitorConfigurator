322f91334a76b10d87f4569b672d8da50b4deed4
Noah <noah.terminello@gmail.com>
2022-08-21 15:43:25 -0700

    can get all display info in windows now

--- main.go
+++ main.go
@@ -4,15 +4,7 @@ import (
 	"fmt"
 	"syscall"
 	"unsafe"
-	/*
-		"image/color"
-
-		"fyne.io/fyne/v2"
-		"fyne.io/fyne/v2/app"
-		"fyne.io/fyne/v2/canvas"
-		"fyne.io/fyne/v2/container"
-		"fyne.io/fyne/v2/layout"
-	*/)
+)
 
 const (
 	//enum display device consts
@@ -26,10 +18,29 @@ const (
 var (
 	user32DLL                  = syscall.NewLazyDLL("user32.dll")
 	procChangeDisplaySettingsA = user32DLL.NewProc("ChangeDisplaySettingsA")
-	procEnumDisplayDevicesA    = user32DLL.NewProc("EnumDisplayDevicesA") //params are (&Cstylestring, uint32, &Display_Device, uint32)
+	procEnumDisplayDevicesA    = user32DLL.NewProc("EnumDisplayDevicesA")  //params are (&Cstylestring, uint32, &Display_Device, uint32)
+	procEnumDisplaySettingsA   = user32DLL.NewProc("EnumDisplaySettingsA") //params are &cstrign, uint32, &devmode
+	procEnumDisplayMonitors    = user32DLL.NewProc("EnumDisplayMonitors")  //params are HDC, LPCRECT, MONITORENUMPROC, LPARAM
+	procGetMonitorInfoA        = user32DLL.NewProc("GetMonitorInfoA")      //params are hmonitor, lpmonitorinfo
 )
 
-//some thoughts: The pointer to dispdeva really needs to be to 424 bytes of contiguous memory. currently no guarantees of that. will be good for parsing data, but thats it
+type SettingConfigList struct {
+	monitor      DispDevA
+	settingsList []DevMode //list of configurations for the given monitor
+}
+
+type Monitors struct {
+	hmonitor []syscall.Handle
+	hdc      []syscall.Handle
+	rect     []*Rect
+}
+
+type Rect struct {
+	left   int32
+	top    int32
+	right  int32
+	bottom int32
+}
 
 //must set cb when calling EnumDispDevA
 type DispDevA struct {
@@ -39,6 +50,15 @@ type DispDevA struct {
 	StateFlags   uint32
 	DeviceID     [128]uint8
 	DeviceKey    [128]uint8
+}
+
+func dumpDispDev(dd *DispDevA) {
+	fmt.Println("Size: ", dd.cb)
+	fmt.Println("DeviceName: ", string(dd.DeviceName[0:]))
+	fmt.Println("DeviceString: ", string(dd.DeviceString[0:]))
+	fmt.Println("StateFlags: ", dd.StateFlags)
+	fmt.Println("DeviceID: ", string(dd.DeviceID[0:]))
+	fmt.Println("DeviceKey: ", string(dd.DeviceKey[0:]))
 }
 
 //https://docs.microsoft.com/en-us/windows/win32/api/windef/ns-windef-pointl
@@ -65,35 +85,47 @@ type DevMode struct {
 	dmDriverExtra   uint16
 	dmFields        uint32
 	//I have determined that since I will only be using this for display devices, I only need to include DUMMYSTRUCTNAME2 for this union here, so I will be doing that
+	//TODO: DETERMINE WHETHER OR NOT SIZEOFUNION IS JUST THE LARGEST MEMBER
 	dummyUnionName dummyStructName2
 	//note, I have determined short to be a 2 byte type, so int16, relevant comment because we have some shorts (will comment which are shorts)
-	dmColor         int16
-	dmDuplex        int16
-	dmYResolution   int16
-	dmTTOption      int16
-	dmCollate       int16
-	dmFormName      [CCHFORMNAME]byte
-	dmLogPixels     uint16
-	dmBitsPerPel    uint32
-	dmPelsWidth     uint32
-	dmPelsHeight    uint32
-	dummyUnionName2 uint32 //can be either dmDisplayFlags, or more likely, dmNup <-- change display settings uses this!
+	dmColor            int16
+	dmDuplex           int16
+	dmYResolution      int16
+	dmTTOption         int16
+	dmCollate          int16
+	dmFormName         [CCHFORMNAME]byte
+	dmLogPixels        uint16
+	dmBitsPerPel       uint32
+	dmPelsWidth        uint32
+	dmPelsHeight       uint32
+	dummyUnionName2    uint32 //can be either dmDisplayFlags, or more likely, dmNup <-- change display settings uses this! //either way, uint32
+	dmDisplayFrequency uint32
+	//the rest of these do not have to be declared, this is why yhe size is neccessary
+	dmICMMethod     uint32
+	dmICMIntent     uint32
+	dmMediaType     uint32
+	dmDitherType    uint32
+	dmReserved1     uint32
+	dmReserved2     uint32
+	dmPanningWidth  uint32
+	dmPanningHeight uint32
+}
 
-	/*
-	  union {
-	    DWORD dmDisplayFlags;
-	    DWORD dmNup;
-	  } DUMMYUNIONNAME2;
-	  DWORD dmDisplayFrequency;
-	  DWORD dmICMMethod;
-	  DWORD dmICMIntent;
-	  DWORD dmMediaType;
-	  DWORD dmDitherType;
-	  DWORD dmReserved1;
-	  DWORD dmReserved2;
-	  DWORD dmPanningWidth;
-	  DWORD dmPanningHeight;
-	*/
+type MonitorInfo struct {
+	cbSize    uint32
+	rcMonitor Rect
+	rcWork    Rect
+	dwFlags   uint32
+}
+
+func dumpDevMode(dm *DevMode) {
+	//this only dumps "important values" lol
+	fmt.Println("dmDeviceName: ", string(dm.dmDeviceName[0:]))
+	fmt.Println("dmSize: ", dm.dmSize)
+	fmt.Println("dmBitsPerPel: ", dm.dmBitsPerPel)
+	fmt.Println("dmPelsWiddth: ", dm.dmPelsWidth)
+	fmt.Println("dmPelsHeight: ", dm.dmPelsHeight)
+	fmt.Println("dmDisplayFrequency: ", dm.dmDisplayFrequency)
 }
 
 // StringToCharPtr converts a Go string into pointer to a null-terminated cstring.
@@ -121,15 +153,6 @@ func enumDispDev(lpDevice string, iDevNum uint32, lpDisplayDevice *DispDevA, dwF
 	}
 
 	return r1
-}
-
-func dumpDispDev(dd *DispDevA) {
-	fmt.Println("Size: ", dd.cb)
-	fmt.Println("DeviceName: ", string(dd.DeviceName[0:]))
-	fmt.Println("DeviceString: ", string(dd.DeviceString[0:]))
-	fmt.Println("StateFlags: ", dd.StateFlags)
-	fmt.Println("DeviceID: ", string(dd.DeviceID[0:]))
-	fmt.Println("DeviceKey: ", string(dd.DeviceKey[0:]))
 }
 
 //returns an array containing all attached devices to any display adapter
@@ -210,25 +233,151 @@ func getActiveMonitors() []DispDevA {
 	return result
 }
 
+//this func is a wrapper for EnumDisplaySettingsA windows api function.
+//we use it to get the possible settings for a display
+func enumDisplaySettings(lpszDeviceName string, iModeNum uint32, lpDevMode *DevMode) uintptr {
+	lpDevMode.dmSize = uint16(unsafe.Sizeof(lpDevMode))
+
+	r1, _, err := procEnumDisplaySettingsA.Call(uintptr(unsafe.Pointer(StringToCharPtr(lpszDeviceName))),
+		uintptr(iModeNum),
+		uintptr(unsafe.Pointer(lpDevMode)))
+	if err != syscall.Errno(0) {
+		fmt.Println("error: ", err)
+	}
+	return r1
+}
+
+//should get list of possible settings
+//if I want to optimize this, I might create a smaller structure than Devmode to fill this
+func getPossibleSettingsForMonitor(lpszDeviceName string) []DevMode {
+	result := make([]DevMode, 0)
+
+	var fail bool = false
+	for x := 0; !fail; x++ {
+		temp := DevMode{}
+		num := enumDisplaySettings(lpszDeviceName, uint32(x), &temp)
+		if num != 0 {
+			result = append(result, temp)
+		} else {
+			fail = true
+		}
+	}
+	return result
+}
+
+//@return each key is a monitor and then all its possible configurations as the value
+func getSettingsConfigList(monitors *[]DispDevA) []SettingConfigList {
+	result := make([]SettingConfigList, 1)
+	for i := range *monitors {
+		temp := SettingConfigList{(*monitors)[i], getPossibleSettingsForMonitor(string((*monitors)[i].DeviceName[0:]))}
+		result = append(result, temp)
+	}
+	return result
+}
+
+//I can enumerate the monitors I need here.
+//the callback function should be called per monitor
+//it's a little janky, and not giving me position info, so we use GetMonitor now!
+func enumDisplayMonitors() Monitors {
+	monin := Monitors{make([]syscall.Handle, 0), make([]syscall.Handle, 0), make([]*Rect, 0)}
+	monproccallback := syscall.NewCallback(monitorEnumProc)
+
+	r1, _, err := procEnumDisplayMonitors.Call(uintptr(unsafe.Pointer(nil)),
+		uintptr(unsafe.Pointer(nil)),
+		uintptr(unsafe.Pointer(monproccallback)),
+		uintptr(unsafe.Pointer(&monin)))
+	if r1 == 0 {
+		fmt.Println("Failed. R1: ", r1)
+	}
+	if err != syscall.Errno(0) {
+		fmt.Println("error: ", err)
+	}
+
+	return monin
+}
+
+//callback function for enumDIsplayMonitors
+func monitorEnumProc(hMonitor syscall.Handle, hdc syscall.Handle, rect *Rect, inform *Monitors) uintptr {
+	inform.hdc = append(inform.hdc, hdc)
+	inform.hmonitor = append(inform.hmonitor, hMonitor)
+	inform.rect = append(inform.rect, rect)
+	return 500 //random value lol
+}
+
+//wrapper for you know it, GetMonitorInfoA
+//i do not know if i need to pass handles with unsafe.Pointer, but assuming i dont for right now
+func GetMonitorInfo(hMonitor syscall.Handle) MonitorInfo {
+	temp := MonitorInfo{}
+	temp.cbSize = uint32(unsafe.Sizeof(temp))
+
+	r1, _, err := procGetMonitorInfoA.Call(uintptr(hMonitor), uintptr(unsafe.Pointer(&temp)))
+	if r1 == 0 {
+		fmt.Println("Failed. R1: ", r1)
+	}
+	if err != syscall.Errno(0) {
+		fmt.Println("error: ", err)
+	}
+	return temp
+}
+
 func main() {
-	/* BASE FUNCTIONALITY
-	cb := DispDevA{}
-	enumDispDev("", 0, &cb, 0x00000001)
-	dumpDispDev(&cb)
+	/*
+		//BASE FUNCTIONALITY
+		//basic test for calling enumDispDev
+		cb := DispDevA{}
+		enumDispDev("", 0, &cb, 0x00000001)
+		dumpDispDev(&cb)
 	*/
 
 	/*
+		//lists all display adapters
 		mons := shallowQueryDisplays()
 		for i := range mons {
 			dumpDispDev(&mons[i])
 		}
 	*/
 
+	//gets all active monitor
+	mons := getActiveMonitors()
+	for i := range mons {
+		dumpDispDev(&mons[i])
+	}
+
 	/*
-		mons := getActiveMonitors()
-		for i := range mons {
-			dumpDispDev(&mons[i])
+		//basic test for calling enumDispSettings
+		monitors := getActiveMonitors()
+		dm := DevMode{}
+		fmt.Println(dm)
+		r1 := enumDisplaySettings(string(monitors[0].DeviceName[0:]), 0, &dm)
+		fmt.Println("test, r1:", r1)
+		fmt.Println(dm)
+	*/
+
+	/*
+		//test for getting all settings of a monitor
+		monitors := getActiveMonitors()
+		settings := getPossibleSettingsForMonitor(string(monitors[0].DeviceName[0:]))
+		for i := range settings {
+			dumpDevMode(&settings[i])
 		}
 	*/
+
+	/*
+		//testing getting all possible settings for all monitors
+		monitors := getActiveMonitors()
+		list := getSettingsConfigs(&monitors)
+		fmt.Println(list)
+	*/
+
+	/*
+		//simple test of enum display monitors.
+		//TODO: fix the rectangle
+		info := enumDisplayMonitors()
+		fmt.Println(*(info.rect[0]))
+	*/
+
+	//test for display position
+	fmt.Println(GetMonitorInfo(enumDisplayMonitors().hmonitor[0]).rcWork)
+	fmt.Println(GetMonitorInfo(enumDisplayMonitors().hmonitor[1]).rcWork)
 
 }
